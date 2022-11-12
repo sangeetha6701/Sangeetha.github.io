41d841e65c4b5399865eec52651a9728733442b1
sangeetha6701 <74009069+sangeetha6701@users.noreply.github.com>
2022-11-12 14:53:43 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,56 @@
+/*education*/
+(function () {
+  "use strict";
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
+
+
+  function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
+  function increase() {
+    // Change the variable to modify the speed of the number increasing from 0 to (ms)
+    let SPEED = 40;
+    // Retrieve the percentage value
+    let limit = parseInt(document.getElementById("value").innerHTML, 10);
+
+    for(let i = 0; i <= limit; i++) {
+        setTimeout(function () {
+            document.getElementById("value").innerHTML = i + "%";
+        }, SPEED * i);
+    }
+}
+
+increase();
